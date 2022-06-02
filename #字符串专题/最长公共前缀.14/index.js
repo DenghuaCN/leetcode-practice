e835fccfb3978cfe74804d3c93a1c50a76cb87b7
denghua <denghua.fm@foxmail.com>
2022-06-02 11:38:17 +0800

    [*]daily practice

--- #字符串专题/最长公共前缀.14/index.js
+++ #字符串专题/最长公共前缀.14/index.js
@@ -21,37 +21,39 @@
 */
 
 
-let example = ["flower","flow","flight"]
-
 /**
+ * 暴力查找(On^2)
  * @param {string[]} strs
  * @return {string}
  */
 let longestCommonPrefix = function(strs) {
-  // 判定两种情况，结果集存在两种，1. 不存在任何长度至少为1的公共前缀字符串 2. 存在至少长度为1的公共前缀字符串
+  // 判定情况，结果集存在两种，1. 不存在任何长度至少为1的公共前缀字符串 2. 存在至少长度为1的公共前缀字符串
 
-  strs.sort((a, b) => {return a.length - b.length}) // 按字符串长度生序排列
+  // 按字符串长度升序处理输入集合
+  strs.sort((a, b) => {return a.length - b.length})
 
   let commonPreFix = [];
-  let shortestStr = strs[0]; // 遍历最短字符串，每轮循环确定一个公共字符，最多 存在的公共前缀字符串 将等于 集合中最短的字符串
+  let shortestStr = strs[0]; // 遍历最短字符串，每轮循环确定一个公共字符
 
   for (let i = 0; i < shortestStr.length; i++) {
     let everyTimeBinary = shortestStr[i];
 
     for (let j = 0; j < strs.length; j++) {
-      // 如果本轮循环有一个字符不想等，则返回
+      // 如果本轮循环有一个字符不匹配，则返回
       if (everyTimeBinary !== strs[j][i]) {
         return commonPreFix.join('');
       }
     }
-    // 每一轮内部循环结束，确定一个字符是否都存在于所有字符串集合中
-    commonPreFix.push(everyTimeBinary);
+    // 一轮内部循环结束，确定一个字符是否都存在于所有字符串集合中
+    commonPreFix[i] = everyTimeBinary;
   }
 
   return commonPreFix.join('');
 };
 
 
-let result = longestCommonPrefix(example);
+// let input = ["dog","racecar","car"];
+let input = ["flower","flow","flight"]
+let result = longestCommonPrefix(input);
 
 console.log(result);
